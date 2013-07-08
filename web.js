dfc742fe3aff2f9337e6caf6c337bbaa4c9c5741
BC <bc@bc-Aspire-5250.(none)>
2013-07-07 21:05:20 -0400

    removed  fs.readFileSync

--- web.js
+++ web.js
@@ -2,9 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFileSync('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
+app.get('/', function(request, response) {
+  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
