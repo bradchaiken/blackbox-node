f8da107d27620c7ca6a85b46a70063b204f39f6d
BC <bc@bc-Aspire-5250.(none)>
2013-07-07 21:04:09 -0400

    fs.readFileSync

--- web.js
+++ web.js
@@ -2,8 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
+fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
 });
 
 var port = process.env.PORT || 5000;
