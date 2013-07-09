2e5cc98dc8853b7278d3fbfde586fd985b6bfec4
BC <bc@bc-Aspire-5250.(none)>
2013-07-08 23:01:31 -0400

    Buffer.toStrng

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+	var buffer = new Buffer(fs.readFileSync('index.html'));
+	response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
